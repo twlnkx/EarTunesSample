08a32568b6b362c11ea25e06a9ff842bcedd2f65
twlnkx <twinkle.ascano@tup.edu.ph>
2023-11-19 17:27:34 +0800

    app

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const cookie = require('cookie-parser')
 const cors = require('cors')
-const products = require('./routes/controller/productContoller');
-
+// const products = require('./routes/controller/productContoller');
+const products = require("./routes/product");
 const auth = require('./routes/auth')
 
-app.use(cors({
-    origin: "http://localhost:4012",
-    methods: ["GET","POST","PUT","DELETE"], 
-}))
+app.use(function(req,res,next){
+    res.set({
+        "Access-Control-Allow-Origin": "http://localhost:4012",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS" 
+    })
+})
+app.use(cors())
 app.use(express.json({limit:'50mb'}));
 
 app.use(express.urlencoded({limit: "50mb", extended: true }));
@@ -17,3 +20,5 @@ app.use(cookie());
 
 app.use('/api/v1', auth);
 app.use('/api/v1',products);
+
+module.exports = app;
