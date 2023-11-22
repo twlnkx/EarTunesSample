8f187e99069c02eeecf932995eac8c49f4a2595e
twlnkx <twinkle.ascano@tup.edu.ph>
2023-11-22 17:59:19 +0800

    almost done checkout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,8 @@ import "react-toastify/dist/ReactToastify.css";
 import Cart from './Components/Cart/Cart';
 import Shipping from './Components/Cart/Shipping';
 import ConfirmOrder from './Components/Cart/ConfirmOrder';
+import Payment from "./Components/Cart/Payment";
+import OrderSuccess from "./Components/Cart/OrderSuccess";
 
 function App() {
   // fetch("api/register",{
@@ -104,7 +106,8 @@ function App() {
           <Route path="/cart" element={<Cart cartItems={state.cartItems} addItemToCart = {addItemToCart} removeItemFromCart = {removeItemFromCart}/>} exact="true" />
           <Route path="/shipping" element={<Shipping shipping={state.shippingInfo} saveShippingInfo={saveShippingInfo} />} />
           <Route path="/confirm" element={<ConfirmOrder cartItems={state.cartItems} shippingInfo={state.shippingInfo} />} />
-          {/* <Route path="/payment" element={<Payment cartItems={state.cartItems} shippingInfo={state.shippingInfo} />} /> */}
+          <Route path="/payment" element={<Payment cartItems={state.cartItems} shippingInfo={state.shippingInfo} />} />
+          <Route path="/success" element={<OrderSuccess />} />
 
         </Routes>
       </Router>
